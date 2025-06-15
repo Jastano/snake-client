8dcca31bd074e4f6844ef557c250d1fbfc3d0357
JasTano <jasmine.broodhagen@outlook.com>
2025-06-14 22:37:17 -0400

    comments added

--- input.js
+++ input.js
@@ -17,6 +17,12 @@ const handleUserInput = function (key) {
   if (key === 'd') {
     connection.write("Move: right");
   }
+  if (key === "z") {
+    connection.write("Say: Hello world")
+  }
+  if (key === "x") {
+    connection.write("Say: im super fast!")
+  }
 };
 
 const setupInput = function (conn) {
